7ff38e21d18afebd6693416bb4fd2c7932e422fd
Julien Tanguy <tanguy.j@sfeir.com>
2024-02-08 09:49:44 +0000

    feat: update pg init scripts

--- steps/04-scripts-solution/db/init.sql
+++ steps/04-scripts-solution/db/init.sql
@@ -1,11 +1,12 @@
-create table workshop (
+create table workshops (
     id serial primary key,
     conference text unique not null,
     city text not null,
+    given_at date not null,
     created_at timestamptz default now()
 );
 
-insert into workshop (conference, city) values 
-    ('Volcamp', 'Clermont-Ferrand'),
-    ('Devfest', 'Nantes'),
-    ('Touraine Tech', 'Tours');
+insert into workshops (conference, city, given_at) values 
+    ('Volcamp', 'Clermont-Ferrand', '2023-10-12'),
+    ('Devfest', 'Nantes', '2023-10-19'),
+    ('Touraine Tech', 'Tours', '2024-02-09');
